491ab049d357b1ff7e9a0b774871efe8257cc044
WellingtonDeveloper <wellington.developer.caldeira@gmail.com>
2022-06-09 10:48:37 -0300

    Gestão de Clientes em C#
    * Menu parte 1

--- Projeto2/Program.cs
+++ Projeto2/Program.cs
@@ -8,8 +8,26 @@ namespace Projeto2
 {
     class Program
     {
+        enum Menu { Listagem = 1, Adicionar = 2, Remover = 3, Sair = 4}
         static void Main(string[] args)
         {
+          
+            Console.WriteLine("Sistema de clientes - Bem vindo!");
+            Console.WriteLine("1 - Listagem\n2 - Adicionar\n3 - Remover\n4 - Sair");
+            int intOp = int.Parse(Console.ReadLine());
+            Menu opcao = (Menu)intOp;
+
+            switch (opcao)
+            {
+                case Menu.Adicionar:
+                    break;
+                case Menu.Listagem:
+                    break;
+                case Menu.Remover:
+                    break;
+                case Menu.Sair:
+                    break;
+            }
         }
     }
 }
